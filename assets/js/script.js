4eab60a17b29b4cd45ac6befbabdfdee73b49619
Rocio Caro <rcarocasanueva@gmail.com>
2024-03-04 13:12:16 -0300

    Ajustes varios en tooltip, mensaje de alerta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
+//activar tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(function () {
+    //popup mensaje enviado
+    $("#enviarCorreo").on("click", function (event) {
+        alert("Muchas gracias, hemos recibido tu mensaje")
+    });
+})
